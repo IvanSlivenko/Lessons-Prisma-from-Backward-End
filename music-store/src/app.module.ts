e0258701790b999da1c30bd9c9ffba42260cf5a7
IvanSlivenko <ivan8822@ukr.net>
2025-09-22 16:06:33 +0300

    22-09-2025-16-06

--- music-store/src/app.module.ts
+++ music-store/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CatalogModule } from './catalog/catalog.module';
+import { PrismaModule } from './prisma';
 
 @Module({
-  imports: [CatalogModule],
+  imports: [PrismaModule, CatalogModule],
 })
 export class AppModule {}
--- music-store/src/catalog/authors/author.service.ts
+++ music-store/src/catalog/authors/author.service.ts
@@ -1,36 +1,46 @@
-import { Injectable, NotImplementedException, Param } from '@nestjs/common';
+import { Injectable, NotImplementedException } from '@nestjs/common';
 import {
   CreateAuthorDTO,
-  GetAuthorParams,
   ReadAuthorDTO,
   ReadManyAuthorsDTO,
   ReadManyAuthorsQueryDTO,
 } from './dto';
+import { PrismaService } from 'src/prisma';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class AuthorsService {
+  constructor(private readonly prisma: PrismaService) {}
+
   getMany(query: ReadManyAuthorsQueryDTO): Promise<ReadManyAuthorsDTO> {
     throw new NotImplementedException(
-      `Method not implemented ${JSON.stringify(query)}`,
+      `Method getMany not implemented ${JSON.stringify(query)}`,
     );
   }
 
   getOne(authorId: string): Promise<ReadAuthorDTO> {
-    throw new NotImplementedException(`Method not implemented ${authorId}`);
+    throw new NotImplementedException(
+      `Method getOne not implemented ${authorId}`,
+    );
   }
 
-  create(data: CreateAuthorDTO): Promise<string> {
-    throw new NotImplementedException(
-      `Method not implemented ${JSON.stringify(data)}`,
-    );
+  async create(data: CreateAuthorDTO): Promise<string> {
+    const { id } = await this.prisma.author.create({
+      data: { ...data, id: randomUUID() },
+    });
+    return id;
   }
 
   update(authorId: string, data: CreateAuthorDTO): Promise<void> {
     console.log(data);
-    throw new NotImplementedException(`Method not implemented ${authorId}`);
+    throw new NotImplementedException(
+      `Method update not implemented ${authorId}`,
+    );
   }
 
   delete(authorId: string): Promise<void> {
-    throw new NotImplementedException(`Method not implemented ${authorId}`);
+    throw new NotImplementedException(
+      `Method delete not implemented ${authorId}`,
+    );
   }
 }
